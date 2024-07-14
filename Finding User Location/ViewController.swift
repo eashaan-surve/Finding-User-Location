31d81440371e0bee54a419daa98c51ffc8ec017b
Pranav Krishnan <pranavkrish2005.gmail.com>
2024-07-14 15:31:07 -0700

    fixed zoom

--- Finding User Location/ViewController.swift
+++ Finding User Location/ViewController.swift
@@ -18,7 +18,6 @@ class CustomAnnotation: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
     var title: String?
     var useUserLocationStyle: Bool
-    
     init(coordinate: CLLocationCoordinate2D, title: String?, useUserLocationStyle: Bool) {
         self.coordinate = coordinate
         self.title = title
@@ -31,7 +30,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     @IBOutlet var map: MKMapView!
     
     var locationManager = CLLocationManager()
-    var timer: Timer?
+    var currentUserLocation: CLLocationCoordinate2D?
+    var writeTimer: Timer?
+    var readTimer: Timer?
+    var isOperationActive = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,7 +63,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         }
 
             Task {
-                await self.handleFirestoreOperations(latitude: latitude, longitude: longitude, db: db)
+                self.handleFirestoreOperations(latitude: latitude, longitude: longitude, db: db)
             }
     }
     
@@ -78,64 +80,86 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         return nil
     }
-    
-    func handleFirestoreOperations(latitude: CLLocationDegrees, longitude: CLLocationDegrees, db: Firestore) async {
-        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { timer in
+
+        func handleFirestoreOperations(latitude: CLLocationDegrees, longitude: CLLocationDegrees, db: Firestore) {
             Task {
-                do {
-                    try await db.collection("location").document("User1").setData([
-                        "latitude": latitude,
-                        "longitude": longitude
-                    ])
-                    print("Document successfully written!")
-                } catch {
-                    print("Error writing document: \(error)")
-                }
-
+                await startWriteOperation(latitude: latitude, longitude: longitude, db: db)
             }
-            let x = 1
-            if x == 10 {
-                timer.invalidate()
+            
+            Task {
+                await startReadOperation(db: db)
             }
         }
-
-        let docRef = db.collection("location").document("User2")
-        var dataDescription: [String: Any]? = nil
         
-        do {
-            let document = try await docRef.getDocument()
-            if document.exists {
-                dataDescription = document.data()
-                print("Document data: \(dataDescription)")
-            } else {
-                print("Document does not exist")
+        func startWriteOperation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, db: Firestore) async {
+            while isOperationActive {
+                await writeLocationToFirestore(latitude: latitude, longitude: longitude, db: db)
+                try? await Task.sleep(nanoseconds: 5_000_000_000) // 5 seconds
             }
-        } catch {
-            print("Error getting document: \(error)")
         }
         
-        if let data = dataDescription {
-            if let latitude = data["latitude"] as? Double, let longitude = data["longitude"] as? Double {
-                let latDelta: CLLocationDegrees = 0.05
-                let lonDelta: CLLocationDegrees = 0.05
-                let span2: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-                let location2: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                let region2: MKCoordinateRegion = MKCoordinateRegion(center: location2, span: span2)
-                
-                DispatchQueue.main.async {
-                    self.addAnnotationAtLocation(location: location2)
-                    self.updateMapRegionToShowAllAnnotations()
-                }
+        func startReadOperation(db: Firestore) async {
+            while isOperationActive {
+                await readLocationFromFirestore(db: db)
+                try? await Task.sleep(nanoseconds: 5_000_000_000) // 5 seconds
             }
-            else {
-                    print("Latitude or longitude not found or not of expected type")
+        }
+        
+        func writeLocationToFirestore(latitude: CLLocationDegrees, longitude: CLLocationDegrees, db: Firestore) async {
+            do {
+                try await db.collection("location").document("User1").setData([
+                    "latitude": latitude,
+                    "longitude": longitude
+                ])
+                print("Document successfully written!")
+                await MainActor.run {
+                    self.currentUserLocation = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
                 }
-            } else {
-                print("No document data available")
+            } catch {
+                print("Error writing document: \(error)")
             }
+        }
+        
+        func readLocationFromFirestore(db: Firestore) async {
+            do {
+                let docRef = db.collection("location").document("User2")
+                let document = try await docRef.getDocument()
+                if let data = document.data(),
+                   let latitude = data["latitude"] as? Double,
+                   let longitude = data["longitude"] as? Double {
+                    let location2 = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                    
+                    await MainActor.run {
+                        self.addAnnotationAtLocation(location: location2)
+                        self.updateMapRegionToShowAllAnnotations()
+                    }
+                    
+                    if let currentLocation = await MainActor.run(body: { self.currentUserLocation }) {
+                        let distance = await MainActor.run { self.distanceBetween(coord1: currentLocation, coord2: location2) }
+                        if distance < 0.05 {
+                            await MainActor.run {
+                                self.isOperationActive = false
+                            }
+                            print("Locations are within 0.05 miles. Stopping database operations.")
+                        }
+                    }
+                }
+            } catch {
+                print("Error getting document: \(error)")
+            }
+        }
+    func distanceBetween(coord1: CLLocationCoordinate2D, coord2: CLLocationCoordinate2D) -> Double {
+        let location1 = CLLocation(latitude: coord1.latitude, longitude: coord1.longitude)
+        let location2 = CLLocation(latitude: coord2.latitude, longitude: coord2.longitude)
+        let distanceInMeters = location1.distance(from: location2)
+        return distanceInMeters / 1609.344
     }
 
     func addAnnotationAtLocation(location: CLLocationCoordinate2D) {
+        if let existingAnnotation = map.annotations.first(where: { $0.title == "Second Location" }) {
+            map.removeAnnotation(existingAnnotation)
+        }
+
         let annotation = CustomAnnotation(coordinate: location, title: "Second Location", useUserLocationStyle: true)
         self.map.addAnnotation(annotation)
     }
@@ -147,4 +171,5 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         }
         map.showAnnotations(annotations, animated: true)
     }
+        
 }
